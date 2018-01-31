69961cac81bf9945465bc114afb6975425383337
darkdread <sakatagintoki9001@gmail.com>
2018-02-01 00:50:57 +0800

    pause menu color transition on button hover

---
+++ Assets/ButtonTextHover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonTextHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    private Text text;
+    public Color onExit;
+    public Color onHover;
+    public float transitionDuration;
+
+    private void Start() {
+        text = transform.GetChild(0).GetComponent<Text>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        text.CrossFadeColor(onHover, transitionDuration, true, true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        text.CrossFadeColor(onExit, transitionDuration, true, true);
+    }
+}
